4fe09b8e5fe7a86b2ee6a6068c43e21e30af1b4d
erm <erm@datalore.us>
2013-08-16 21:52:15 +0200

    working on the encryption file

--- _encrypt.py
+++ _encrypt.py
@@ -4,31 +4,46 @@ import binascii
 import os
 
 
-key = '1234567891111111'
-iv = Random.get_random_bytes(16)
+class Crypter(object):
 
-def generate_key():
-    key = os.urandom(16).encode('base_64')
-    return key
+    def __init__(self, key):
+        self.key = key
+        self.iv = '\x00'*16
 
-def encrypt(txt_str):
-    encoded_AES = AES.new(key, AES.MODE_CFB, iv)
-    ciphertext = encoded_AES.encrypt(txt_str)
-    return ciphertext.encode('hex')
-    
-def decrypt(hex_str):
-    ciphertext = binascii.unhexlify(hex_str)
-    decoded_AES = AES.new(key, AES.MODE_CFB, iv)
-    txt_str = decoded_AES.decrypt(ciphertext)
-    return txt_str
+    def encrypt(self, txt_str):
+        encoded_AES = AES.new(self.key, AES.MODE_CFB, self.iv)
+        ciphertext = encoded_AES.encrypt(txt_str)
+        return ciphertext.encode('hex')
+        
+    def decrypt(self, hex_str):
+        ciphertext = binascii.unhexlify(hex_str)
+        decoded_AES = AES.new(self.key, AES.MODE_CFB, self.iv)
+        txt_str = decoded_AES.decrypt(ciphertext)
+        return txt_str
 
-def test():
+
+def generate_key(log=True):
+    key = os.urandom(16)
+    if log:
+        print("This is your secret key.  / ! \ IMPORTANT / ! \: do not lose/forget it!")
+        print(key)
+    else:
+        return key
+
+
+def test(cr):
     test_str = "Testing encryption/decription of text!"
-    hex_str = encrypt(plain_text)
-    decrypted_str = decrypt(hex_str).rstrip()
+    hex_str = cr.encrypt(test_str)
+    decrypted_str = cr.decrypt(hex_str).rstrip()
     if decrypted_str == test_str:
         print("[!] SUCCESS: Test successful!")
     else:
         print("[!] FAILURE: Test was not successful!")
+
+if __name__ == "__main__":
+    test_key = generate_key(log=False)
+    cr = Crypter(test_key)
+    test(cr)
+
         
-test()
+#test()
