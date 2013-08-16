c91a39996338c48e36bcb4549541c17722ee9d8e
erm <erm@datalore.us>
2013-08-16 22:25:55 +0200

    changes

--- snowmail/controllers/dashboard.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, request, redirect, url_for, \
-    render_template, session
-
-mod = Blueprint('dashboard', __name__)
-
-
-def index():
-    return render_template('dashboard/index.html')
